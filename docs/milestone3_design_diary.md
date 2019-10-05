faada8e5bb458dfe4e46606ef8def2b9fbc639cc
bac18 <bricor14@gmail.com>
2019-10-05 03:06:02 -0700

    it has been a great challenge to cohesively connect the vector with the characters being displayed on the screen. though i feel like i am right on the edge of figuring out, and with time and energy it will come. im playing around with a buffer that is temporary, and pushes onto the main vector only when needed. this will no doubt take me longer than the due date to figure out. Milestone 3 submission.

--- docs/milestone3_design_diary.md
+++ docs/milestone3_design_diary.md
@@ -1,2 +1,17 @@
 # Design Diary
+
 Use this space to talk about your process.  For inspiration, see [my prompts](../../../docs/sample_reflection.md) 
+
+figuring out how to "connect" the underlying data structure with the
+temporary surface display of characters on the window is challenging.
+it involves a lot of playing around with indexing vectors. i think the
+best way to save changes is that whenever something changes the screen
+(ie scrolling down, up, save button), save the current screen to a vector.
+a challenge is noticing changes when necessary, as well as when new text
+is typed somewhere, i have to go to the correct line(s) and column(s) and
+then go to the correct place in the string in the line(s).
+
+it has been an incredible challenge and very frustrating at times working with vectors. 
+fully accomplishing this milestone will take me beyond the due date without a doubt.
+
+
--- main.cpp
+++ main.cpp
@@ -12,13 +12,23 @@
 
 #define PDC_DLL_BUILD 1
 #include <cstdlib>
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <ctime>
 #include <vector>
+#include "curses.h"
+#include "panel.h"
 using namespace std;
 
-//void print_vect_from_pos(WINDOW* win, int page_num, int win_cursy, int win_cursx, vector<string> source_vect);
+// takes a window, windows y- and x-cursor, the "page" to display (offset of height of window), vector of lines
+void print_vect_page(WINDOW* win, int cursor_y, int cursor_x, int page_num, vector<vector<string>> pages_vect);
+
+vector<char> push_buff_elements();
+void make_new_page(vector<vector<string>>& current_pages, int &total);
+void set_overflow(bool& overflow, int num_of_pages);
+string chvect_to_str(vector<char> char_vector);
+void flush_buffer(vector<char>& buffer_vect);
 
 int main(int argc, char* argv[])
 {
@@ -51,130 +61,79 @@ int main(int argc, char* argv[])
 		wrefresh(option_bar_windows[i]);
 	}
 
-	vector<string> lines_vect;
+	vector<char> buffer = push_buff_elements();			// has 25 positions
+	int buffer_index = 0;
+	vector<string> lines;			// each has 25 positions
+	int lines_index = 0;
+	vector<vector<string>> pages;	// will be added as needed
+	int page_index = 0;
+	int total_num_pages = 0;
+
+	make_new_page(pages, total_num_pages);
+
+	vector<string> main_win_lines_vect;
+	int main_win_lines_vect_index = 0;
+
+	string open_file;
 	bool overFlow = false;
-	int page = 0;
 
 	if (argc == 2)
 	{
 		ifstream infile;
 		infile.open(argv[1]);
-		if (infile.is_open())
+		if (infile.good() == true)
 		{
 			string output;
+			open_file = argv[1];
 
 			main_win_cursx = 1;
 			main_win_cursy = 1;
+
+			page_index = 0;
+			lines_index = 0;
 			while (getline(infile, output))
-				lines_vect.push_back(output);				// add each line in text file to lines_vect
+			{
+				pages[page_index][lines_index++] = output;				// add each line in text file to lines_vect
+				if (lines_index == 25)
+				{
+					lines_index = 0;
+					make_new_page(pages, total_num_pages);
+					page_index++;
+				}
+			}
 
-			for (int i = (25 * page); i < lines_vect.size(); i++)		// print each string in source_vect to win according to page number
-				mvwprintw(main_window, main_win_cursy++, main_win_cursx, lines_vect[i].c_str());
-			/*print_vect_from_pos(main_window, page, main_win_cursy, main_win_cursx, lines_vect);*/
-
-			if (lines_vect.size() >= ((LINES - 5) * (page + 1)))			// if text is larger than window size, overflow = true
-				overFlow = true;
-
-			main_win_cursy = 1;
-			wmove(main_window, main_win_cursy, main_win_cursx);
+			set_overflow(overFlow, total_num_pages);
 		}
 		infile.close();
+
+		main_win_cursy = 1;
+		main_win_cursx = 1;
+		page_index = 0;
+		lines_index = 0;
+		print_vect_page(main_window, main_win_cursy, main_win_cursx, page_index, pages);
+		wrefresh(main_window);
 	}
 
-	WINDOW* page_indicator_win = newwin(3, 9, LINES - 3, COLS - 9);
-	mvwprintw(page_indicator_win, 1, 2, "pg. %d", page + 1);
-	box(page_indicator_win, '|', '-');
-	wrefresh(page_indicator_win);
-	
-	vector<char> main_win_lines_vect;
 	bool stillRunning = true;
 	while (stillRunning)
 	{
+		WINDOW* page_indicator_win = newwin(3, 9, LINES - 3, COLS - 9);
+		mvwprintw(page_indicator_win, 1, 2, "pg. %d", page_index + 1);
+		box(page_indicator_win, '|', '-');
+		wrefresh(page_indicator_win);
+
 		int user_input = wgetch(main_window);
 
 		switch (user_input)
 		{
 		case '0':
-			stillRunning = false;				// sentinel: breaks out of program
+		{
+			// sentinel: breaks out of program
+			stillRunning = false;
 			break;
-		case KEY_RIGHT:
-			if (main_win_cursx < COLS - 2)		// moves cursor right if it's not already all the way to the right of the window
-			{
-				wmove(main_window, main_win_cursy, ++main_win_cursx);
-				wrefresh(main_window);
-			}
-			break;
-		case KEY_LEFT:
-			if (main_win_cursx > 1)			// moves cursor left if it's not already all the way to the left
-			{
-				wmove(main_window, main_win_cursy, --main_win_cursx);
-				wrefresh(main_window);
-			}
-			break;
-		case KEY_DOWN:
-			if (main_win_cursy < main_win_height)		// moves cursor down if it's not already all the way at the bottom
-			{
-				wmove(main_window, ++main_win_cursy, main_win_cursx);
-				wrefresh(main_window);
-			}
-			else if (main_win_cursy == (LINES - 5))
-			{
-				if (overFlow == true)
-				{
-					main_win_cursy = 1;
-					main_win_cursx = 1;
-					wmove(main_window, main_win_cursy, main_win_cursx);
-					wclrtobot(main_window);
-					box(main_window, '|', '-');
-					page++;
-
-					/*print_vect_from_pos(main_window, ++page, main_win_cursy, main_win_cursx, lines_vect);*/
-					for (int i = (25 * page); i < lines_vect.size(); i++)
-					{
-						mvwprintw(main_window, main_win_cursy++, main_win_cursx, lines_vect[i].c_str());
-
-						/*if (i == lines_vect.size() - 3)*/
-						if (i == (LINES - 6) + (25 * page))
-							break;
-					}
-					main_win_cursy = 1;
-					main_win_cursx = 1;
-					wmove(main_window, main_win_cursy, main_win_cursx);
-				}
-			}
-			break;
-		case KEY_UP:
-			if (main_win_cursy > 1)			// moves cursor up if it's not already all the way at the top
-			{
-				wmove(main_window, --main_win_cursy, main_win_cursx);
-				wrefresh(main_window);
-			}
-			else if (main_win_cursy == 1)
-			{
-				if ((overFlow == true) and (page != 0))
-				{
-					main_win_cursy = 1;
-					main_win_cursx = 1;
-					wmove(main_window, main_win_cursy, main_win_cursx);
-					wclrtobot(main_window);
-					box(main_window, '|', '-');
-					page--;
-
-					for (int i = (25 * page); i < lines_vect.size(); i++)
-					{
-						mvwprintw(main_window, main_win_cursy++, main_win_cursx, lines_vect[i].c_str());
-
-						/*if (i == lines_vect.size() - 3)*/
-						if (i == (LINES - 6) + (25 * page))
-							break;
-					}
-					main_win_cursy = main_win_height;
-					main_win_cursx = 1;
-					wmove(main_window, main_win_cursy, main_win_cursx);
-				}
-			}
-			break;
+		}
 		case KEY_SDOWN:		// displays "open file" box in bottom right
+		{
 			WINDOW* temp_window = newwin(3, 30, 27, 50);
 			keypad(temp_window, TRUE);
 			box(temp_window, '|', '-');
@@ -183,15 +142,16 @@ int main(int argc, char* argv[])
 			int temp_win_height;
 			int temp_win_width;
 			getmaxyx(temp_window, temp_win_height, temp_win_width);
+
 			int temp_win_cursx = 1;
 			int temp_win_cursy = 1;
 			mvwprintw(temp_window, temp_win_cursy, temp_win_cursx, "Open file: ");
 			int temp_win_vect_index = 0;
 
-			vector<char> file_name;
-
 			temp_win_cursx += 11;
 			int c = mvwgetch(temp_window, temp_win_cursy, temp_win_cursx);
+
+			vector<char> file_name;
 			while (c != KEY_SUP)
 			{
 				if ((c == KEY_DC) and (temp_win_cursx > 12))
@@ -232,6 +192,7 @@ int main(int argc, char* argv[])
 			}
 
 			string sfile_name(file_name.begin(), file_name.end());		// make file_name vector into string sfile_name
+			open_file = sfile_name;
 
 			werase(temp_window);
 			wrefresh(temp_window);
@@ -239,7 +200,7 @@ int main(int argc, char* argv[])
 
 			ifstream infile;
 			infile.open(sfile_name);
-			if (infile.is_open())
+			if (infile.good() == true)
 			{
 				string output;
 
@@ -249,21 +210,27 @@ int main(int argc, char* argv[])
 				wclrtobot(main_window);
 				box(main_window, '|', '-');
 				wrefresh(main_window);
+
+				page_index = 0;
+				lines_index = 0;
 				while (getline(infile, output))
-					lines_vect.push_back(output);				// add each line in text file to lines_vect
-
-				for (int i = (25 * page); i < (25 * page) + 25; i++)	// print each string in source_vect to win according to page number
 				{
-					if (lines_vect.size() == i)
-						break;
-					mvwprintw(main_window, main_win_cursy++, main_win_cursx, lines_vect[i].c_str());
+					pages[page_index][lines_index++] = output;				// add each line in text file to lines_vect
+					if (lines_index == 25)
+					{
+						lines_index = 0;
+						make_new_page(pages, total_num_pages);
+						page_index++;
+					}
 				}
-				
-				wrefresh(main_window);
-				/*print_vect_from_pos(main_window, page, main_win_cursy, main_win_cursx, lines_vect);*/
 
-				if (lines_vect.size() >= ((LINES - 5) * (page + 1)))
-					overFlow = true;
+				page_index = 0;
+				lines_index = 0;
+				print_vect_page(main_window, main_win_cursy, main_win_cursx, page_index, pages);
+
+				wrefresh(main_window);
+
+				set_overflow(overFlow, total_num_pages);
 
 				main_win_cursy = 1;
 				wmove(main_window, main_win_cursy, main_win_cursx);
@@ -272,22 +239,203 @@ int main(int argc, char* argv[])
 			infile.close();
 			break;
 		}
-		/*main_win_lines_vect.push_back(user_input);
-		for (int i = 0; i < main_win_lines_vect.size(); i++)
+		case KEY_SUP:
 		{
-			mvwprintw(main_window, main_win_cursy, main_win_cursx++, "%d", user_input);
-		}*/
-		if ((user_input != '0') and (user_input != KEY_RIGHT) and (user_input != KEY_LEFT) and 
-			(user_input != KEY_UP) and (user_input != KEY_DOWN) and (user_input != KEY_SDOWN))
+			make_new_page(pages, total_num_pages);
+			set_overflow(overFlow, total_num_pages);
+			break;
+		}
+		case ALT_9:
+		{
+			ofstream outfile;
+			outfile.open(open_file);
+			if (outfile.good() == true)
+			{
+				outfile.clear();
+				page_index = 0;
+				lines_index = 0;
+
+				for (int i = 0; i < 25; i++)
+				{
+					outfile << pages[page_index][i] << endl;
+					if (i == 24)
+					{
+						lines_index = 0;
+						page_index++;
+					}
+				}
+			}
+			outfile.close();
+			break;
+		}
+		case KEY_DC:
+		{
+			if (main_win_cursx > 1)
+			{
+				mvwdelch(main_window, main_win_cursy, --main_win_cursx);
+				mvwdelch(main_window, main_win_cursy, main_win_width - 2);
+				box(main_window, '|', '-');
+				wmove(main_window, main_win_cursy, --main_win_cursx);
+				wrefresh(main_window);
+				pages[page_index][lines_index].erase(pages[page_index][lines_index].begin() + buffer_index);
+				buffer_index--;
+			}
+		}
+		case KEY_RIGHT:
+		{
+			// moves cursor right if it's not already all the way to the right of the window
+			if (main_win_cursx < COLS - 2)
+			{
+				wmove(main_window, main_win_cursy, ++main_win_cursx);
+				wrefresh(main_window);
+				buffer_index++;
+				break;
+			}
+		}
+		case KEY_LEFT:
+		{
+			// moves cursor left if it's not already all the way to the left
+			if (main_win_cursx > 1)
+			{
+				wmove(main_window, main_win_cursy, --main_win_cursx);
+				wrefresh(main_window);
+				buffer_index--;
+			}
+			break;
+		}
+		case KEY_DOWN:
+		{
+			// moves cursor down if it's not already all the way at the bottom, else if it is and overflow is true, then scroll
+			if (main_win_cursy < main_win_height)
+			{
+				wmove(main_window, ++main_win_cursy, main_win_cursx);
+				main_win_lines_vect_index++;
+				wrefresh(main_window);
+				lines_index++;
+				flush_buffer(buffer);
+			}
+			else if (main_win_cursy == (LINES - 5))
+			{
+				if (overFlow)
+				{
+
+
+					main_win_cursy = 1;
+					main_win_cursx = 1;
+					wmove(main_window, main_win_cursy, main_win_cursx);
+					wclrtobot(main_window);
+					box(main_window, '|', '-');
+
+					print_vect_page(main_window, main_win_cursy, main_win_cursx, ++page_index, pages);
+
+					main_win_cursy = 1;
+					main_win_cursx = 1;
+					lines_index = 0;
+					flush_buffer(buffer);
+					buffer_index = 0;
+					wmove(main_window, main_win_cursy, main_win_cursx);
+
+					main_win_lines_vect_index++;
+				}
+			}
+			break;
+		}
+		case KEY_UP:
+		{
+			// moves cursor up if it's not already all the way at the top
+			if (main_win_cursy > 1)
+			{
+				wmove(main_window, --main_win_cursy, main_win_cursx);
+				wrefresh(main_window);
+				lines_index--;
+				flush_buffer(buffer);
+			}
+			else if (main_win_cursy == 1)
+			{
+				if (overFlow and (page_index != 0))
+				{
+					main_win_cursy = 1;
+					main_win_cursx = 1;
+					wmove(main_window, main_win_cursy, main_win_cursx);
+					wclrtobot(main_window);
+					box(main_window, '|', '-');
+					page_index--;
+
+					print_vect_page(main_window, main_win_cursy, main_win_cursx, page_index, pages);
+
+					main_win_cursy = main_win_height;
+					main_win_cursx = 1;
+					lines_index = 24;
+					flush_buffer(buffer);
+					buffer_index = 0;
+					wmove(main_window, main_win_cursy, main_win_cursx);
+				}
+			}
+			break;
+		}
+		}
+		
+
+		if ((user_input != '0') and (user_input != KEY_RIGHT) and (user_input != KEY_LEFT) and
+			(user_input != KEY_UP) and (user_input != KEY_DOWN) and (user_input != KEY_SDOWN) and 
+			(user_input != KEY_SUP) and (user_input != ALT_9) and (user_input != KEY_DC))
+		{
 			mvwaddch(main_window, main_win_cursy, main_win_cursx++, user_input);
+			buffer[buffer_index++] = user_input;
+			string this_line = chvect_to_str(buffer);
+			pages[page_index][lines_index] += this_line;
+		}
 	}
-	wgetch(main_window);
+
 	endwin();
 	return 0;
 }
 
-//void print_vect_from_pos(WINDOW* win, int page_num, int win_cursy, int win_cursx, vector<string> source_vect)
-//{
-//	for (int i = (25 * page_num); i < ((LINES - 5) * (page_num + 1)); i++)	// print each string in source_vect to win according to page number
-//		mvwprintw(win, win_cursy++, win_cursx, source_vect[i].c_str());
-//}
+void print_vect_page(WINDOW* win, int cursor_y, int cursor_x, int page_num, vector<vector<string>> pages_vect)
+{
+	for (int i = 0; i < 25; i++)	// print each string in source_vect to win according to page number
+	{
+		mvwprintw(win, cursor_y++, cursor_x, pages_vect[page_num][i].c_str());
+	}
+}
+
+vector<char> push_buff_elements()
+{
+	vector<char> line_vect;
+	for (int i = 0; i < 118; i++)
+	{
+		line_vect.push_back(' ');
+	}
+	return line_vect;
+}
+
+void make_new_page(vector<vector<string>>& current_pages, int &total)
+{
+	vector<string> new_lines;
+	for (int i = 0; i < 25; i++)
+	{
+		new_lines.push_back("");
+	}
+	current_pages.push_back(new_lines);
+	total++;
+}
+
+void set_overflow(bool &overflow, int num_of_pages)
+{
+	if (num_of_pages > 0)
+		overflow = true;
+	else
+		overflow = false;
+}
+
+string chvect_to_str(vector<char> char_vector)
+{
+	string string_line(char_vector.begin(), char_vector.end());
+	return string_line;
+}
+
+void flush_buffer(vector<char>& buffer_vect)
+{
+	buffer_vect.clear();
+	buffer_vect = push_buff_elements();
+}
